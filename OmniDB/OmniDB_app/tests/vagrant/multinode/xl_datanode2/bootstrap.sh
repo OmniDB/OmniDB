aebac74a8e514168d9dfea7bc9fd4fed0235a63e
William Ivanski <william.ivanski@gmail.com>
2017-10-10 12:00:52 -0300

    Finished implementing support for pglogical

--- OmniDB/OmniDB_app/tests/vagrant/multinode/xl_datanode2/bootstrap.sh
+++ OmniDB/OmniDB_app/tests/vagrant/multinode/xl_datanode2/bootstrap.sh
@@ -84,7 +84,7 @@ echo "max_connections = 100" >> "$PG_CONF"
 echo "hot_standby = off" >> "$PG_CONF"
 echo "pooler_port = 40102" >> "$PG_CONF"
 echo "gtm_host = '192.168.56.105'" >> "$PG_CONF"
-echo "gtm_port = 6666"" >> "$PG_CONF"
+echo "gtm_port = 6666" >> "$PG_CONF"
 
 # XL specific authentication
 echo "local   all           postgres                          trust" >> "$PG_HBA"

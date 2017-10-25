9dd544a0813b53dbfbf8dd99dc494306e68c8e9a
William Ivanski <william.ivanski@gmail.com>
2017-10-25 16:39:22 -0200

    First version of deploy app for openSUSE

---
+++ OmniDB/deploy/opensuse_app_amd64/bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+
+PYTHON_VERSION=3.5.2
+
+echo "Installing dependencies..."
+zypper install -y gcc gcc-c++ make patch libopenssl-devel readline-devel libbz2-devel rpm-build
+zypper install -y gnome-shell
+echo "Done"
+
+echo "Installing pyenv..."
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+echo "Done"
+
+echo "Installing Python $PYTHON_VERSION..."
+env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+echo "Done"
+
+echo "Cloning OmniDB repo..."
+rm -rf ~/OmniDB
+git clone https://github.com/OmniDB/OmniDB ~/OmniDB
+cd ~/OmniDB
+git checkout dev
+echo "Done"
+
+echo "Installing OmniDB dependencies..."
+pip install pip --upgrade
+pip install -r ~/OmniDB/requirements.txt
+pip install -r ~/OmniDB/OmniDB/deploy/requirements_for_deploy_app.txt
+echo "Done"
+
+echo "Building..."
+cd ~/OmniDB/OmniDB/deploy/opensuse_app_amd64/
+./build.sh
+echo "Done"

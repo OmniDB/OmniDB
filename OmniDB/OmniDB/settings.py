913a78374a4b9afb9cb6cebf7f09a265e0d3183d
Rafael Castro <rafael.castro@2ndquadrant.com>
2017-12-06 08:23:10 -0500

    Fixing version

--- OmniDB/OmniDB/settings.py
+++ OmniDB/OmniDB/settings.py
@@ -204,8 +204,8 @@ LOGGING = {
 }
 
 #OMNIDB PARAMETERS
-OMNIDB_VERSION             = 'OmniDB 2.5.0'
-OMNIDB_SHORT_VERSION       = '2.5.0'
+OMNIDB_VERSION             = 'OmniDB 2.4.0'
+OMNIDB_SHORT_VERSION       = '2.4.0'
 BINDKEY_EXECUTE            = 'alt+q'
 BINDKEY_EXECUTE_MAC        = 'ctrl+q'
 BINDKEY_REPLACE            = 'ctrl+g'

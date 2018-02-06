93cc4b7c74a166431e769386410f00cd2355ae02
William Ivanski <william.ivanski@gmail.com>
2018-02-06 09:46:03 -0200

    Preparing deployment machines to bundle OIC

--- OmniDB/deploy/server_centos_amd64/bootstrap.sh
+++ OmniDB/deploy/server_centos_amd64/bootstrap.sh
@@ -3,7 +3,12 @@
 PYTHON_VERSION=3.5.2
 
 echo "Installing dependencies..."
-yum install -y gcc gcc-c++ make git patch openssl-devel zlib-devel readline-devel sqlite-devel bzip2-devel rpm-build
+yum install -y gcc gcc-c++ make git patch openssl-devel zlib-devel readline-devel sqlite-devel bzip2-devel rpm-build wget
+echo "Done"
+
+echo "Installing p7zip..."
+wget https://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/6/x86_64/Packages/p/p7zip-16.02-2.el6.x86_64.rpm
+rpm -U --quiet p7zip-16.02-2.el6.x86_64.rpm
 echo "Done"
 
 echo "Installing pyenv..."
@@ -22,6 +27,11 @@ echo "Done"
 echo "Cloning OmniDB repo..."
 rm -rf ~/OmniDB
 git clone --depth 1 --branch dev https://github.com/OmniDB/OmniDB ~/OmniDB
+echo "Done"
+
+echo "Downloading OIC..."
+wget https://github.com/OmniDB/OracleInstantClient/raw/master/oic/linux_x64.7z
+7za x linux_x64.7z
 echo "Done"
 
 echo "Building..."

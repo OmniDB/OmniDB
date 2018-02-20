c4df3af36cefbc79e6d7aae6bf605ffc10e7e574
Rafael Castro <rafael.castro@2ndquadrant.com>
2018-02-20 10:47:18 -0500

    Updating Dockerfile.

--- OmniDB/OmniDB/settings.py
+++ OmniDB/OmniDB/settings.py
@@ -18,7 +18,7 @@ import string
 import getpass
 
 # Development Mode
-DEV_MODE = False
+DEV_MODE = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

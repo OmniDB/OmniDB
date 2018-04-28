7263082c43d81fe41010eda2863649a09e61dd1f
Rafael Castro <rafael.castro@2ndquadrant.com>
2018-04-28 15:14:34 -0400

    Adding omnidb_app electron solution.

---
+++ omnidb_app/main.js
@@ -0,0 +1,52 @@
+const electron = require('electron')
+const child_process = require('child_process')
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+var ipc = require('electron').ipcMain;
+let django;
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600, icon: path.join(__dirname, 'images/omnidb.png'), title: 'OmniDB'});
+  mainWindow.setMenu(null);
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  //django = child_process.spawn(path.join(__dirname, 'omnidb-server/omnidb-server'),[],{detached: true});
+  django = child_process.spawn('python3',['/home/rafaelthca/Repositories/github/rafaelthca/OmniDB/OmniDB/omnidb-server.py','-A'],{detached: true});
+  django.stdout.on('data', (data) => {
+    v_data_list = data.toString('utf8').split("\n");
+    mainWindow.webContents.send('info' , v_data_list);
+  });
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  //if (process.platform !== 'darwin') {
+  process.kill(django.pid);
+  app.quit();
+  //}
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})

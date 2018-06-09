8119bdd970e47435e68c83dbe2634291147727f6
William Ivanski <william.ivanski@gmail.com>
2018-06-09 12:44:09 -0300

    Small improvements to plugin system

--- OmniDB/OmniDB_app/static/plugins/test_plugin/plugin.py
+++
@@ -1,3 +0,0 @@
-def test_func(p_database_object, p_data):
-    result = p_database_object.v_connection.ExecuteScalar('select version()')
-    return { 'version': result }
--- OmniDB/OmniDB_app/views/plugins.py
+++ OmniDB/OmniDB_app/views/plugins.py
@@ -13,36 +13,41 @@ import importlib
 from configparser import ConfigParser
 from itertools import chain
 
+import OmniDB_app.include.OmniDatabase as OmniDatabase
+
+PLUGINS_ROOT = 'OmniDB_app/plugins'
+PLUGINS_STATIC_ROOT = 'OmniDB_app/static/plugins'
+
 #loading python plugins
 plugins = {}
-plugins_folders = listdir('OmniDB_app/static/plugins')
+plugins_folders = listdir(PLUGINS_ROOT)
 for plugin_folder in plugins_folders:
     plugin_name = ''
     plugin_version = ''
-    if isfile(join('OmniDB_app/static/plugins',plugin_folder,'plugin.conf')):
+    if isfile(join(PLUGINS_ROOT,plugin_folder,'plugin.conf')):
         conf_exists = True
     else:
         conf_exists = False
-    if isfile(join('OmniDB_app/static/plugins',plugin_folder,'plugin.js')):
+    if isfile(join(PLUGINS_STATIC_ROOT,plugin_folder,'plugin.js')):
         js_exists = True
     else:
         js_exists = False
-    if isfile(join('OmniDB_app/static/plugins',plugin_folder,'plugin.py')):
+    if isfile(join(PLUGINS_ROOT,plugin_folder,'plugin.py')):
         py_exists = True
     else:
         py_exists = False
     #if is directory, try to import plugin.py inside it
-    if isdir(join('OmniDB_app/static/plugins',plugin_folder)):
+    if isdir(join(PLUGINS_ROOT,plugin_folder)):
         try:
             parser = ConfigParser()
-            with open(join('OmniDB_app/static/plugins',plugin_folder,'plugin.conf')) as lines:
+            with open(join(PLUGINS_ROOT,plugin_folder,'plugin.conf')) as lines:
                 lines = chain(("[top]",), lines)
                 parser.read_file(lines)
                 plugin_name = parser.get('top', 'name')
                 plugin_version = parser.get('top', 'version')
 
             plugins[plugin_name] = {
-                'module'         : importlib.import_module('OmniDB_app.static.plugins.{0}.plugin'.format(plugin_folder)),
+                'module'         : importlib.import_module('OmniDB_app.plugins.{0}.plugin'.format(plugin_folder)),
                 'folder'         : plugin_folder,
                 'name'           : plugin_name,
                 'version'        : plugin_version,
@@ -52,7 +57,9 @@ for plugin_folder in plugins_folders:
                 'javascript_file': '/static/plugins/{0}/plugin.js'.format(plugin_folder),
                 'plugin_folder'  : '/static/plugins/{0}/'.format(plugin_folder)
             }
+            print('Loaded plugin {0}'.format(plugin_name))
         except Exception as exc:
+            print('Failed to load plugin {0}: {1}'.format(plugin_name, str(exc)))
             plugins[plugin_name] = {
                 'module'         : None,
                 'folder'         : plugin_folder,
@@ -128,7 +135,6 @@ def list_plugins(request):
 
         plugin_list.append([plugin['folder'],plugin['name'],plugin['version'],conf_html,js_html,py_html, plugin_enabled])
 
-
     v_return['v_data'] = plugin_list
 
     return JsonResponse(v_return)
@@ -157,7 +163,18 @@ def exec_plugin_function(request):
     p_tab_id = json_object['p_tab_id']
 
     try:
-        v_database = v_session.v_tab_connections[p_tab_id]
+        v_database_orig = v_session.v_tab_connections[p_tab_id]
+        v_database = OmniDatabase.Generic.InstantiateDatabase(
+            v_database_orig.v_db_type,
+            v_database_orig.v_connection.v_host,
+            str(v_database_orig.v_connection.v_port),
+            v_database_orig.v_service,
+            v_database_orig.v_user,
+            v_database_orig.v_connection.v_password,
+            v_database_orig.v_conn_id,
+            v_database_orig.v_alias,
+            'OmniDB / {0}'.format(p_plugin_name)
+        )
     except:
         v_database = None
 
@@ -169,6 +186,11 @@ def exec_plugin_function(request):
             v_return['v_error'] = True
             return JsonResponse(v_return)
 
-    v_return['v_data'] = getattr(plugins[p_plugin_name]['module'], p_function_name)(v_database,p_data)
+    try:
+        v_return['v_data'] = getattr(plugins[p_plugin_name]['module'], p_function_name)(v_database,p_data)
+    except Exception as exc:
+        v_return['v_data'] = {'password_timeout': True, 'message': str(exc) }
+        v_return['v_error'] = True
+        return JsonResponse(v_return)
 
     return JsonResponse(v_return)

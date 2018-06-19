5b93db3be0d55008cf63aaea5c6d79d1b230422a
Rafael Castro <rafael.castro@2ndquadrant.com>
2018-06-19 10:45:42 -0400

    Adjusting Plugin API documentation and README for deployment.

--- OmniDB/OmniDB_app/static/OmniDB_app/js/PluginHook.js
+++ OmniDB/OmniDB_app/static/OmniDB_app/js/PluginHook.js
@@ -10,47 +10,149 @@ OmniDB is distributed in the hope that it will be useful, but WITHOUT ANY WARRAN
 You should have received a copy of the GNU General Public License along with OmniDB. If not, see http://www.gnu.org/licenses/.
 */
 
-//Plugin API functions
+/*
+# Plugin API
 
-//activateHook(): enables a specific hook
-// ** AVAILABLE HOOKS **
-// innerTabMenu: Used to insert custom options in the + internal tab
-// outerTabMenu: Used to insert custom options in the + external tab
-// windowResize: Called every time window is resized, including when internal objects are resized
-// changeTheme: Called when theme is changed
-// postgresqlTreeNodeOpen: After opening a postgresql tree node
-// postgresqlTreeContextMenu: Used to insert custom options in the current postgresql tree node
-// postgresqlTreeNodeClick: After clicking on postgresql tree node
-// oracleTreeNodeOpen: After opening a oracle tree node
-// oracleTreeContextMenu: Used to insert custom options in the current oracle tree node
-// oracleTreeNodeClick: After clicking on oracle tree node
-// mysqlTreeNodeOpen: After opening a mysql tree node
-// mysqlTreeContextMenu: Used to insert custom options in the current mysql tree node
-// mysqlTreeNodeClick: After clicking on mysql tree node
-// mariadbTreeNodeOpen: After opening a mariadb tree node
-// mariadbTreeContextMenu: Used to insert custom options in the current mariadb tree node
-// mariadbTreeNodeClick: After clicking on mariadb tree node
+## Javascript functions
 
-//callPluginFunction(): calls a specific function of a specific plugin (python backend)
+- ```activateHook(p_hook, p_function)```
+  - **DESCRIPTION**: Enables a specific hook.
+  - **PARAMETERS**:
+    - p_hook: the name of hook to be activated.
+    - p_function: the function to be called whenever that hook is triggered. Depending on the hook, this function is called with specific arguments and expects a specific returning value.
+  - **AVAILABLE HOOKS**:
+    - innerTabMenu: Used to insert custom options in the + internal tab.
+      - **MUST RETURN**: List of menu itens (Check test_plugin for example).
+    - outerTabMenu: Used to insert custom options in the + external tab.
+      - **MUST RETURN**: List of menu itens (Check test_plugin for example).
+    - windowResize: Called every time window is resized, including when internal objects are resized
+    - changeTheme: Called when theme is changed.
+      - **PARAMETERS**:
+        - p_editor_theme: The name of the new selected theme.
+        - p_theme_type: The type of the new theme (dark or light).
+    - postgresqlTreeNodeOpen: After opening a postgresql tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+    - postgresqlTreeContextMenu: Used to insert custom options in the current postgresql tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+      - **MUST RETURN**: List of menu itens (Check test_plugin for example).
+    - postgresqlTreeNodeClick: After clicking on postgresql tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+    - oracleTreeNodeOpen: After opening a oracle tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+    - oracleTreeContextMenu: Used to insert custom options in the current oracle tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+      - **MUST RETURN**: List of menu itens (Check test_plugin for example).
+    - oracleTreeNodeClick: After clicking on oracle tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+    - mysqlTreeNodeOpen: After opening a mysql tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+    - mysqlTreeContextMenu: Used to insert custom options in the current mysql tree node.
+      - **MUST RETURN**: List of menu itens (Check test_plugin for example).
+    - mysqlTreeNodeClick: After clicking on mysql tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+    - mariadbTreeNodeOpen: After opening a mariadb tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+    - mariadbTreeContextMenu: Used to insert custom options in the current mariadb tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
+      - **MUST RETURN**: List of menu itens (Check test_plugin for example).
+    - mariadbTreeNodeClick: After clicking on mariadb tree node.
+      - **PARAMETERS**:
+        - p_node: Tree node object.
 
-//createInnerTab(): creates an internal blank tab
+- ```p_node.createChildNode(p_text,p_expanded,p_icon,p_tag)```
+  - **DESCRIPTION**: Creates a child node in the current node. This function is supposed to be called with hooks that contain p_node as parameter.
+  - **PARAMETERS**:
+    - p_text: node text.
+    - p_expanded: whether to expand the node when creating it. This is useful when creating childs of childs.
+    - p_icon: path of an image to be used in the tab title. Use together with getPluginPath() to get the correct relative path.
+    - p_tag: sets a tag for the child node. This is useful to create custom attributes that will be used to identify this node when other hooks are triggered.
 
-//createOuterTab(): creates an external blank tab
+- ```callPluginFunction({ p_plugin_name: '', p_function_name: '', p_data: null, p_callback: null, p_loading: true, p_check_database_connection: true })```
+  - **DESCRIPTION**: Asynchronously calls a specific python function of a specific plugin (python backend).
+  - **PARAMETERS**:
+    - p_plugin_name: the name of the plugin.
+    - p_function_name: the name of the function being called.
+    - p_data: data to be sent to the python function.
+    - p_callback: javascript function to be called when the function ends.
+    - p_loading: whether to show the loading image.
+    - p_check_database_connection: whether to check if the database connection is working before calling the python function.
 
-//getSelectedInnerTabTag(): gets the tag of the selected internal tab, allowing to store information there
+- ```createInnerTab({ p_name: '', p_image: '', p_select_function: null, p_before_close_function: null })```
+  - **DESCRIPTION**: Creates an internal blank tab.
+  - **PARAMETERS**:
+    - p_name: the name of the tab.
+    - p_image: path of an image to be used in the tab title. Use together with getPluginPath() to get the correct relative path.
+    - p_select_function: function to be called whenever the tab is selected.
+    - p_before_close_function: function to be called before the tab is closed.
+  - **RETURNS**: tab object tag.
 
-//getSelectedOuterTabTag(): gets the tag of the selected external tab, allowing to store information there
+- ```createOuterTab({ p_name: '', p_image: '', p_select_function: null, p_before_close_function: null })```
+  - **DESCRIPTION**: Creates an external blank tab.
+  - **PARAMETERS**:
+    - p_name: the name of the tab.
+    - p_image: path of an image to be used in the tab title. Use together with getPluginPath() to get the correct relative path.
+    - p_select_function: function to be called whenever the tab is selected.
+    - p_before_close_function: function to be called before the tab is closed.
+  - **RETURNS**: tab object tag.
 
-//tabSQLTemplate(): creates an internal Query Tab with a specific SQL passed as a parameter
+- ```getSelectedInnerTabTag()```
+  - **DESCRIPTION**: Gets the tag of the selected internal tab, allowing to store information there.
+  - **RETURNS**: Selected internal tab tag.
 
-//getPluginPath(): get the path of the specific plugin to use reference static files
+- ```getSelectedOuterTabTag()```
+  - **DESCRIPTION**: Gets the tag of the selected external tab, allowing to store information there.
+  - **RETURNS**: Selected external tab tag.
 
-//showError(): shows a popup with the specific error message
+- ```createSQLTab({ p_name: '', p_template: '', p_show_qtip: true })```
+  - **DESCRIPTION**: Creates an internal Query Tab with a specific SQL passed as a parameter.
+  - **PARAMETERS**:
+    - p_name: the name of the tab.
+    - p_template: the SQL to be filled in the editor.
+    - p_show_qtip: whether to show a tip with the message "Adjust command and run!"
 
-//setDDL()
+- ```getPluginPath(p_plugin_name)```
+  - **DESCRIPTION**: Get the path of the specific plugin to use reference static files.
+  - **PARAMETERS**:
+    - p_plugin_name: the name of the plugin.
 
-//setProperties
+- ```showError(p_message)```
+  - **DESCRIPTION**: Shows a popup with the specific error message.
 
+- ```setDDL({ p_ddl: '', p_select: true})```
+  - **DESCRIPTION**: Sets the content of the DDL box in the DDL tab.
+  - **PARAMETERS**:
+    - p_ddl: the DDL to be filled in the editor.
+    - p_select: whether to also select the DDL tab.
+
+- ```setProperties({ p_properties: [], p_select: true})```
+  - **DESCRIPTION**: Sets the content of the Properties grid in the Properties tab.
+  - **PARAMETERS**:
+    - p_properties: the properties to be displayed in the grid. This is a list of lists.
+    - p_select: whether to also select the Properties tab.
+
+## Python side
+
+Plugins on the python side are implemented as user defined functions that will be called by
+the javascript API function `callPluginFunction()`. The functions are called always
+with 2 parameters:
+
+- ```my_python_function(p_database_object, p_data)```
+  - **PARAMETERS**:
+    - p_database_object: OmniDB's database object that contains several attributes
+    and functions to retrieve data from the database.
+    - p_data: optional paramater to send data from the javascript side.
+
+*/
 var v_plugins = {}
 
 $(function () {
@@ -250,6 +352,10 @@ function callPluginFunction({ p_plugin_name, p_function_name, p_data = null, p_c
       },
       'box',
       p_loading);
+}
+
+function createSQLTab({ p_name = '', p_template = '', p_show_qtip = true }) {
+  tabSQLTemplate(p_name, p_template, p_show_qtip);
 }
 
 function createInnerTab({ p_name = '', p_image = '', p_select_function = null, p_before_close_function = null }) {

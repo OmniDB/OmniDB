c04803188eda86cdec3a8182b10ce722663c5ea5
William Ivanski <william.ivanski@gmail.com>
2018-10-09 16:56:20 -0300

    Implemented pglogical plugin

--- OmniDB/OmniDB_app/tests/vagrant/pglogical-2-postgresql-10-2nodes/bootstrap.sh
+++ OmniDB/OmniDB_app/tests/vagrant/pglogical-2-postgresql-10-2nodes/bootstrap.sh
@@ -11,7 +11,7 @@ APP_DB_NAME=omnidb_tests
 PG_VERSION=10
 
 # Edit the following to change the local port PostgreSQL port 5432 will be mapped to
-PG_LOCAL_PORT=5408
+PG_LOCAL_PORT=5432
 
 ###########################################################
 # Changes below this line are probably not necessary
@@ -40,50 +40,58 @@ print_db_usage () {
   echo "  PGUSER=$APP_DB_USER PGPASSWORD=$APP_DB_PASS psql -h localhost -p $PG_LOCAL_PORT $APP_DB_NAME"
 }
 
-export DEBIAN_FRONTEND=noninteractive
+#!/bin/bash
 
 PROVISIONED_ON=/etc/vm_provision_on_timestamp
 if [ -f "$PROVISIONED_ON" ]
 then
   echo "VM was already provisioned at: $(cat $PROVISIONED_ON)"
-  echo "To run system updates manually login via 'vagrant ssh' and run 'apt-get update && apt-get upgrade'"
-  echo ""
-  print_db_usage
+  echo "To run system updates manually login via 'vagrant ssh' and run 'yum update'"
   exit
 fi
 
-PG_REPO_APT_SOURCE=/etc/apt/sources.list.d/pgdg.list
-if [ ! -f "$PG_REPO_APT_SOURCE" ]
-then
-  # Add PG apt repo:
-  echo "deb http://apt.postgresql.org/pub/repos/apt/ stretch-pgdg main" > "$PG_REPO_APT_SOURCE"
-
-  # Add PGDG repo key:
-  wget --quiet -O - https://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | apt-key add -
-fi
-
-SQ_REPO_APT_SOURCE=/etc/apt/sources.list.d/2ndquadrant.list
-if [ ! -f "$SQ_REPO_APT_SOURCE" ]
-then
-  # Add 2ndQ apt repo:
-  echo "deb [arch=amd64] http://packages.2ndquadrant.com/pglogical/apt/ stretch-2ndquadrant main" > "$SQ_REPO_APT_SOURCE"
-
-  # Add 2ndQ repo key:
-  wget --quiet -O - http://packages.2ndquadrant.com/pglogical/apt/AA7A6805.asc | sudo apt-key add -
-fi
-
 # Update package list and upgrade all packages
-apt-get update
-apt-get -y upgrade
+yum -y update
 
-apt-get -y install postgresql-10-pglogical
+# Install EPEL repo
+yum -y install epel-release
 
-PG_CONF="/etc/postgresql/$PG_VERSION/main/postgresql.conf"
-PG_HBA="/etc/postgresql/$PG_VERSION/main/pg_hba.conf"
-PG_DIR="/var/lib/postgresql/$PG_VERSION/main"
+# Install dependencies
+yum -y install libicu libxslt
+
+# Install PostgreSQL
+rpm -ivh https://yum.postgresql.org/10/redhat/rhel-7-x86_64/postgresql10-libs-10.4-1PGDG.rhel7.x86_64.rpm
+rpm -ivh https://yum.postgresql.org/10/redhat/rhel-7-x86_64/postgresql10-10.4-1PGDG.rhel7.x86_64.rpm
+rpm -ivh https://yum.postgresql.org/10/redhat/rhel-7-x86_64/postgresql10-server-10.4-1PGDG.rhel7.x86_64.rpm
+rpm -ivh https://yum.postgresql.org/10/redhat/rhel-7-x86_64/postgresql10-contrib-10.4-1PGDG.rhel7.x86_64.rpm
+
+# Install PGDG repo
+rpm -ivh https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm
+
+# Install pglogical repo
+rpm -ivh http://packages.2ndquadrant.com/pglogical/yum-repo-rpms/pglogical-rhel-1.0-3.noarch.rpm
+
+# Install pglogical
+yum -y install postgresql10-pglogical
+
+# Initialize PostgreSQL database
+/usr/pgsql-10/bin/postgresql-10-setup initdb
+
+# Enable PostgreSQL service
+systemctl enable postgresql-10
+
+# Start PostgreSQL service
+systemctl start postgresql-10
+
+PG_CONF="/var/lib/pgsql/$PG_VERSION/data/postgresql.conf"
+PG_HBA="/var/lib/pgsql/$PG_VERSION/data/pg_hba.conf"
+PG_DIR="/var/lib/pgsql/$PG_VERSION/data"
 
 # Edit postgresql.conf to change listen address to '*':
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
+
+# Append to pg_hba.conf to add password auth:
+echo "host    all             all             all                     md5" >> "$PG_HBA"
 
 # Explicitly set default client_encoding
 echo "client_encoding = utf8" >> "$PG_CONF"
@@ -97,25 +105,27 @@ echo "max_replication_slots = 10" >> "$PG_CONF"
 echo "max_worker_processes = 10" >> "$PG_CONF"
 echo "shared_preload_libraries = 'pglogical'" >> "$PG_CONF"
 
-# BDR specific authentication
+# pglogical specific authentication
 echo "local   all           omnidb                            trust" >> "$PG_HBA"
 echo "host    all           omnidb       127.0.0.1/32         trust" >> "$PG_HBA"
 echo "host    all           omnidb       ::1/128              trust" >> "$PG_HBA"
 echo "local   replication   omnidb                            trust" >> "$PG_HBA"
 echo "host    replication   omnidb       127.0.0.1/32         trust" >> "$PG_HBA"
 echo "host    replication   omnidb       ::1/128              trust" >> "$PG_HBA"
-echo "host    all           omnidb       192.168.56.109/32    trust" >> "$PG_HBA"
-echo "host    replication   omnidb       192.168.56.109/32    trust" >> "$PG_HBA"
+echo "host    all           omnidb       10.33.3.114/32       trust" >> "$PG_HBA"
+echo "host    replication   omnidb       10.33.3.114/32       trust" >> "$PG_HBA"
+echo "host    all           omnidb       10.33.3.115/32       trust" >> "$PG_HBA"
+echo "host    replication   omnidb       10.33.3.115/32       trust" >> "$PG_HBA"
 
 # Append to pg_hba.conf to add password auth:
 echo "host    all           all          all                  md5" >> "$PG_HBA"
 
 # Restart so that all new config is loaded:
-service postgresql restart
+systemctl restart postgresql-10
 
 cat << EOF | su - postgres -c psql
 -- Create the database user:
-CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS' SUPERUSER REPLICATION;
+CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS' SUPERUSER;
 
 -- Create the database:
 CREATE DATABASE $APP_DB_NAME WITH OWNER=$APP_DB_USER

cc7befd59d21173f0a139fe2b0e5c43b70c65945
William Ivanski <william.ivanski@gmail.com>
2018-12-17 18:17:08 -0200

    Fixed bug in migration script

--- OmniDB/OmniDB/migrations/omnidb_0.0.0_2.13.0.sql
+++ OmniDB/OmniDB/migrations/omnidb_0.0.0_2.13.0.sql
@@ -612,13 +612,13 @@ CREATE TABLE cgroups (
   cgroup_id integer primary key,
   user_id integer references users (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
   cgroup_name text
-)
+);--omnidb--
 
 CREATE TABLE cgroups_connections (
   cgroup_id integer references cgroups (cgroup_id) ON UPDATE CASCADE ON DELETE CASCADE,
   conn_id integer references connections (conn_id) ON UPDATE CASCADE ON DELETE CASCADE,
   primary key (cgroup_id, conn_id)
-)
+);--omnidb--
 
 CREATE TABLE version (
     ver_id text not null,

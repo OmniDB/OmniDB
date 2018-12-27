37debaddef7ac3a2dcb11fd6ca055e28ecf17f4d
William Ivanski <william.ivanski@gmail.com>
2018-12-27 17:52:56 -0200

    Fixed bug in connections older than 2.8

--- OmniDB/OmniDB/migrations/omnidb_2.13.0_2.14.0.sql
+++ OmniDB/OmniDB/migrations/omnidb_2.13.0_2.14.0.sql
@@ -1,1 +1,8 @@
+UPDATE connections SET ssh_server = '' WHERE ssh_server IS NULL;--omnidb--
+UPDATE connections SET ssh_port = '22' WHERE ssh_port IS NULL;--omnidb--
+UPDATE connections SET ssh_user = '' WHERE ssh_user IS NULL;--omnidb--
+UPDATE connections SET ssh_password = '' WHERE ssh_password IS NULL;--omnidb--
+UPDATE connections SET ssh_key = '' WHERE ssh_key IS NULL;--omnidb--
+UPDATE connections SET use_tunnel = 0 WHERE use_tunnel IS NULL;--omnidb--
+
 UPDATE version SET ver_id = '2.14.0';--omnidb--

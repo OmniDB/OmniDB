becba357423ca1039b8aeffbbaf61f9ffa825c54
rafaelthca@gmail.com <rafaelthca@gmail.com>
2019-06-07 10:34:49 -0300

    Fixing path bug, adding migration.

--- OmniDB/OmniDB/user_database.py
+++ OmniDB/OmniDB/user_database.py
@@ -7,7 +7,7 @@ import OmniDB_app.include.Spartacus.Utils as Utils
 import OmniDB_app.include.OmniDatabase as OmniDatabase
 
 migrations = {
-    '0.0.0': ('2.15.0', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_0.0.0_2.15.0.sql')),
+    '0.0.0': ('2.16.0', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_0.0.0_2.15.0.sql')),
     '2.4.0': ('2.4.1', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_2.4.0_2.4.1.sql')),
     '2.4.1': ('2.5.0', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_2.4.1_2.5.0.sql')),
     '2.5.0': ('2.6.0', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_2.5.0_2.6.0.sql')),
@@ -22,6 +22,7 @@ migrations = {
     '2.14.0': ('2.14.1', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_2.14.0_2.14.1.sql')),
     '2.14.1': ('2.14.2', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_2.14.1_2.14.2.sql')),
     '2.14.2': ('2.15.0', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_2.14.2_2.15.0.sql')),
+    '2.15.0': ('2.16.0', os.path.join(settings.BASE_DIR, 'OmniDB/migrations/omnidb_2.15.0_2.16.0.sql')),
 }
 
 def get_current_version(p_database):

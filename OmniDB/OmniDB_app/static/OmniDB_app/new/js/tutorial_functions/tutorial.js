80cc95178d3419ee8fb582aae2e64c1919138d1a
Luis Felipe Thofehrn Castro <felipebiox@gmail.com>
2020-09-15 17:07:53 -0300

    Improving omnis-ui-assitant.

--- OmniDB/OmniDB_app/static/OmniDB_app/new/js/tutorial_functions/tutorial.js
+++ OmniDB/OmniDB_app/static/OmniDB_app/new/js/tutorial_functions/tutorial.js
@@ -76,6 +76,94 @@ function startTutorial(p_tutorial_name) {
           p_title: 'User Options',
           p_update_delay: 350
         }
+      ],
+      'connections_menu': [
+        {
+          p_clone_target: true,
+          p_message: `
+          <p>This is the outer connections menu. Each connection added becomes a new item in this menu.</p>
+          <p>The menu initially contains.</p>
+          <ul>
+          <li>Connections manager.</li>
+          <li>Welcome, tutorial and useful links.</li>
+          <li>Snippets panel toggler.</li>
+          <li>Add connection.</li>
+          </ul>
+          <p>Let's first <span class="badge badge-info">add a new connection</span>.</p>
+          <p>Please, click on the <i class="fas fa-plus"></i> button.</p>
+          `,
+          p_target: document.getElementsByClassName('omnidb__tab-menu omnidb__tab-menu--primary')[0],
+          p_title: 'Primary menu'
+        },
+        {
+          p_callback_after_update_start: function() {setTimeout(function(){var v_target = document.getElementById('button_new_connection'); window.omnis_ui_assistant.divClonedElement.children[0].classList.remove('ml-2');},50);},
+          p_callback_start: function() {startConnectionManagement();},
+          p_clone_target: true,
+          p_message: `
+          <p>Click on <strong>New Connection</strong>.</p>
+          `,
+          p_next_button: false,
+          p_target: function() {var v_target = document.getElementById('button_new_connection'); return v_target},
+          p_title: 'Add a New Connection',
+          p_update_delay: 600
+        },
+        {
+          p_message: `
+          <p>Select the proper DBMS technology.</p>
+          `,
+          p_target: function() {var v_target = document.getElementById('conn_form_type'); return v_target},
+          p_title: 'Connection Type',
+          p_update_delay: 300
+        },
+        {
+          p_message: `
+          <p>Type a helpful name for the connection.</p>
+          <p>This is used as name reference on many UI areas.</p>
+          <p>i.e: Local dvdrental barman.</p>
+          `,
+          p_target: function() {var v_target = document.getElementById('conn_form_title'); return v_target},
+          p_title: 'Title'
+        },
+        {
+          p_message: `
+          <p>Type the server address. Do not include ports.</p>
+          <p>i.e:127.0.0.1</p>
+          `,
+          p_target: function() {var v_target = document.getElementById('conn_form_server'); return v_target},
+          p_title: 'Server'
+        },
+        {
+          p_message: `
+          <p>Type the port of the server.</p>
+          <p>i.e: PostgreSQL uses 5432 by default, but if you are using pgbouncer, you may want to use 6432 as the entry point.</p>
+          `,
+          p_target: function() {var v_target = document.getElementById('conn_form_port'); return v_target},
+          p_title: 'Port'
+        },
+        {
+          p_message: `
+          <p>Type the name of the database.</p>
+          <p>i.e: postgres, dvdrental.</p>
+          `,
+          p_target: function() {var v_target = document.getElementById('conn_form_database'); return v_target},
+          p_title: 'Database'
+        },
+        {
+          p_message: `
+          <p>Type the name of the user with priviledges to access the database.</p>
+          <p>i.e: postgres.</p>
+          `,
+          p_target: function() {var v_target = document.getElementById('conn_form_user'); return v_target},
+          p_title: 'User'
+        },
+        {
+          p_message: `
+          <p>You may want to hit 'test' before saving the conntion.</p>
+          <p>After that, click save.</p>
+          `,
+          p_target: function() {var v_target = document.getElementById('conn_form_button_test_connection'); return v_target},
+          p_title: 'Test the Connection'
+        }
       ]
     }
     // Selecting a tutorial

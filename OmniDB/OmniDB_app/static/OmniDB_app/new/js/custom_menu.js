7ded3e733554e234319b9f43e64aba8786178b40
Luis Felipe Thofehrn Castro <felipebiox@gmail.com>
2020-09-27 23:28:32 -0300

    Improved custom menu positioning.

--- OmniDB/OmniDB_app/static/OmniDB_app/new/js/custom_menu.js
+++ OmniDB/OmniDB_app/static/OmniDB_app/new/js/custom_menu.js
@@ -80,12 +80,30 @@ function customMenu(p_position,p_menu,p_object) {
                 v_submenus[k].style.display = 'none';
             }
             v_ul.style.display = 'block';
+            v_ul.style['z-index'] = this.aimara_level + 1;
+            // Repositioning the sub-menu if its out of the frame.
+            let v_ul_rect = v_ul.getBoundingClientRect();
+            let v_ul_h_diff = v_ul_rect.x + v_ul_rect.width - window.innerWidth;
+            if (v_ul_h_diff > 0) {
+              v_ul.classList.add('aimara_menu_left');
+            }
+            let v_ul_v_diff = v_ul_rect.y + v_ul_rect.height - window.innerHeight;
+            if (v_ul_v_diff > 0) {
+              v_ul.classList.add('aimara_menu_bottom');
+            }
+            // let v_ul_new_rect = v_ul.getBoundingClientRect();
+            // let v_ul_new_v_diff = v_ul_new_rect.y;
+            // if (v_ul_new_v_diff < 0) {
+            //   v_ul.style.height = v_ul.offsetHeight + v_ul_new_v_diff + 'px';
+            //   v_ul.style['overflow-y'] = 'scroll';
+            // }
           }
 
           v_li.appendChild(v_ul);
           var v_span_more = createSimpleElement('div',null,null);
           v_span_more.appendChild(createImgElement(null,'menu_img',v_url_folder + '/static/OmniDB_app/images/right.png'));
           v_li.appendChild(v_span_more);
+
           customMenuRecursive(p_menu[i].submenu.elements,v_ul,p_object,v_closediv,v_div, 1);
         }
 
@@ -96,10 +114,12 @@ function customMenu(p_position,p_menu,p_object) {
       let v_div_h_diff = v_div_rect.x + v_div_rect.width - window.innerWidth;
       if (v_div_h_diff > 0) {
         v_div.style.left = v_div_rect.x - v_div_h_diff - 5 + 'px';
+        v_div.classList.add('aimara_menu_left');
       }
       let v_div_v_diff = v_div_rect.y + v_div_rect.height - window.innerHeight;
       if (v_div_v_diff > 0) {
         v_div.style.top = v_div_rect.y - v_div_v_diff - 5 + 'px';
+        v_div.classList.add('aimara_menu_bottom');
       }
 }
 
@@ -148,6 +168,23 @@ function customMenuRecursive(p_submenu,p_ul,p_object,p_closediv, p_cm_div, p_lev
             v_submenus[k].style.display = 'none';
         }
         v_ul.style.display = 'block';
+        v_ul.style['z-index'] = this.aimara_level + 1;
+        // Repositioning the sub-menu if its out of the frame.
+        let v_ul_rect = v_ul.getBoundingClientRect();
+        let v_ul_h_diff = v_ul_rect.x + v_ul_rect.width - window.innerWidth;
+        if (v_ul_h_diff > 0) {
+          v_ul.classList.add('aimara_menu_left');
+        }
+        let v_ul_v_diff = v_ul_rect.y + v_ul_rect.height - window.innerHeight;
+        if (v_ul_v_diff > 0) {
+          v_ul.classList.add('aimara_menu_bottom');
+        }
+        // let v_ul_new_rect = v_ul.getBoundingClientRect();
+        // let v_ul_new_v_diff = v_ul_new_rect.y;
+        // if (v_ul_new_v_diff < 0) {
+        //   v_ul.style.height = v_ul.offsetHeight + v_ul_new_v_diff + 'px';
+        //   v_ul.style['overflow-y'] = 'scroll';
+        // }
       }
 
       v_li.appendChild(v_ul);
@@ -156,6 +193,7 @@ function customMenuRecursive(p_submenu,p_ul,p_object,p_closediv, p_cm_div, p_lev
       v_li.appendChild(v_span_more);
       customMenuRecursive(p_submenu[i].submenu.elements,v_ul,p_object,p_closediv,p_cm_div, p_level+1);
     }
-
   })(i);
+
+
 }

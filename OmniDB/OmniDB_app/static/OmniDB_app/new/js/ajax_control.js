7bba5c29085a84141e669290f0c24a1795614c72
rafaelthca <rafael.castro@2ndquadrant.com>
2020-10-09 18:10:12 -0300

    Improving polling.

--- OmniDB/OmniDB_app/static/OmniDB_app/new/js/ajax_control.js
+++ OmniDB/OmniDB_app/static/OmniDB_app/new/js/ajax_control.js
@@ -115,7 +115,7 @@ function cancelAjax() {
 		}
 	}
 	// Setting the token.
- 	var csrftoken = getCookie('omnidb_csrftoken');
+ 	var csrftoken = getCookie(v_csrf_cookie_name);
 	// Requesting data with ajax.
  	v_ajax_call = $.ajax({
  		url: v_url_folder + p_url,

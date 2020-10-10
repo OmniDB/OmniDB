f3c0df0614f8c15d737601bbca5f0d618de06a11
Luis Felipe Thofehrn Castro <felipebiox@gmail.com>
2020-10-10 19:02:57 -0300

    Updated colors on themes for daterangepicker, changed console autocomplete positioning and fixed custom file input weird text.

--- OmniDB/OmniDB_app/static/OmniDB_app/new/js/tab_functions/inner_console_tab.js
+++ OmniDB/OmniDB_app/static/OmniDB_app/new/js/tab_functions/inner_console_tab.js
@@ -92,12 +92,12 @@ var v_createConsoleTabFunction = function() {
   // Setting custom keyboard shortcuts callbacks.
   $('#txt_input_' + v_tab.id).find('.ace_text-input').on('keyup',function(event){
     if (v_connTabControl.selectedTab.tag.enable_autocomplete !== false) {
-      autocomplete_start(v_editor1,0, event);
+      autocomplete_start(v_editor1,1, event);
     }
   });
   $('#txt_input_' + v_tab.id).find('.ace_text-input').on('keydown',function(event){
     if (v_connTabControl.selectedTab.tag.enable_autocomplete !== false) {
-      autocomplete_keydown(v_editor1, event);
+      autocomplete_keydown(v_editor1,1, event);
     }
   });
 

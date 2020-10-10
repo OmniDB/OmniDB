091642bc69f0e220af0470ec91424d6c5e1af9f5
Luis Felipe Thofehrn Castro <felipebiox@gmail.com>
2020-10-10 17:25:41 -0300

    Fixed initial state of connection public toggler when editting.

--- OmniDB/OmniDB_app/static/OmniDB_app/new/js/connections.js
+++ OmniDB/OmniDB_app/static/OmniDB_app/new/js/connections.js
@@ -638,7 +638,7 @@ function editConnection(p_conn_obj) {
   document.getElementById('conn_form_ssh_user').value = p_conn_obj.tunnel.user;
   document.getElementById('conn_form_ssh_password').value = '';
   document.getElementById('conn_form_ssh_key').value = '';
-
+	document.getElementById('conn_form_public').checked = (p_conn_obj.public)
 
 	if (p_conn_obj.password && p_conn_obj.password !== null && p_conn_obj.password !== '') {
 		if ($('#conn_form_user_pass_check_icon').length === 0) {
